c3ed57498ac93fa501b46cb43c952b1550678c36
zhuzhe <zhuzhe@findpiano.cn>
2020-03-15 12:01:29 +0800

    fix

--- antd-pro/src/pages/system/account/components/UpdateForm.tsx
+++ antd-pro/src/pages/system/account/components/UpdateForm.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Modal, Form, Select, message } from 'antd';
+import { Modal } from 'antd';
+import SchemaForm, { useForm, createFormActions, ISchema, IForm } from '@formily/antd';
+import { setup } from '@formily/antd-components';
 import { queryAllRole } from '../service';
 
-const FormItem = Form.Item;
 interface FormProps {
   modalVisible: boolean;
   initVals: any;
@@ -14,18 +15,47 @@ const formLayout = {
   labelCol: { span: 5 },
   wrapperCol: { span: 15 },
 };
+setup();
+const actions = createFormActions();
 
 export default (props: React.PropsWithChildren<FormProps>) => {
   const { modalVisible, onSubmit, onCancel, initVals } = props;
   const [roles, setRoles] = useState<any[]>([]);
-  const [form] = Form.useForm();
   const initialValues = initVals
     ? {
         id: initVals.id,
-        name: initVals.name,
         roles: initVals.roles.map((it: any) => it.id),
       }
     : {};
+
+  const form = useForm({
+    value: initialValues,
+    actions,
+  }) as IForm;
+  const schema: ISchema = {
+    type: 'object',
+    properties: {
+      roles: {
+        type: 'string',
+        title: '角色类型',
+        required: true,
+        'x-props': {
+          placeholder: '请选择',
+          mode: 'multiple',
+        },
+        'x-rules': [
+          {
+            required: true,
+            message: '角色不能为空',
+          },
+        ],
+        enum: roles.map(it => ({
+          value: it.id,
+          label: it.name,
+        })),
+      },
+    },
+  };
   useEffect(() => {
     queryAllRole().then(data => {
       if (data && data.code === 200) {
@@ -35,15 +65,16 @@ export default (props: React.PropsWithChildren<FormProps>) => {
   }, []);
 
   const okHandle = async () => {
-    if (!form.isFieldsTouched()) {
-      message.warn('请修改后提交');
-      return;
-    }
-    const fieldsValue = await form.validateFields();
-    const rs: boolean = await onSubmit(fieldsValue);
-    if (rs) {
-      form.resetFields();
-    }
+    try {
+      await form.validate();
+      await form.submit(async (values: any) => {
+        const rs: boolean = await onSubmit(values);
+        if (rs) {
+          form.reset();
+        }
+      });
+      // eslint-disable-next-line no-empty
+    } catch (error) {}
   };
   return (
     <Modal
@@ -53,35 +84,11 @@ export default (props: React.PropsWithChildren<FormProps>) => {
       title="编辑账户"
       onOk={okHandle}
       onCancel={() => {
-        form.resetFields();
+        form.reset();
         onCancel();
       }}
     >
-      <Form form={form} initialValues={initialValues}>
-        {/* <FormItem
-          {...formLayout}
-          label="账号"
-          name="name"
-          rules={[{ required: true, message: '账号不能为空' }]}
-        >
-          <Input placeholder="请输入" />
-        </FormItem> */}
-        <FormItem
-          {...formLayout}
-          label="角色类型"
-          name="roles"
-          rules={[{ required: true, message: '角色不能为空' }]}
-        >
-          <Select mode="multiple" placeholder="请选择">
-            {roles &&
-              roles.map(it => (
-                <Select.Option key={it.id} value={it.id}>
-                  {it.name}
-                </Select.Option>
-              ))}
-          </Select>
-        </FormItem>
-      </Form>
+      <SchemaForm schema={schema} form={form} {...formLayout} />
     </Modal>
   );
 };
