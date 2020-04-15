9cdb72027fb6ea98246346ed6e834929d167b9b6
zhuzhe <zhuzhe@findpiano.cn>
2020-04-15 15:40:48 +0800

    fix package.json

--- antd-pro/src/pages/system/accountsettings/components/base.tsx
+++ antd-pro/src/pages/system/accountsettings/components/base.tsx
@@ -1,19 +1,13 @@
 import { UploadOutlined } from '@ant-design/icons';
-import { Form } from '@ant-design/compatible';
-import '@ant-design/compatible/assets/index.css';
-import { Button, Input, Upload, message } from 'antd';
+import { Button, Input, Upload, Form, message } from 'antd';
 import { FormattedMessage, formatMessage } from 'umi';
 import React, { Component, Fragment } from 'react';
 
-import { FormComponentProps } from '@ant-design/compatible/es/form';
 import { connect } from 'dva';
 import { CurrentUser } from '../data.d';
 // import GeographicView from './GeographicView';
 // import PhoneView from './PhoneView';
 import styles from './BaseView.less';
-
-const FormItem = Form.Item;
-// const { Option } = Select;
 
 // 头像组件 方便以后独立，增加裁剪之类的功能
 const AvatarView = ({ avatar }: { avatar: string }) => (
@@ -71,27 +65,12 @@ const AvatarView = ({ avatar }: { avatar: string }) => (
 //   callback();
 // };
 
-interface BaseViewProps extends FormComponentProps {
+interface BaseViewProps {
   currentUser?: CurrentUser;
 }
 
 class BaseView extends Component<BaseViewProps> {
   view: HTMLDivElement | undefined = undefined;
-
-  componentDidMount() {
-    this.setBaseInfo();
-  }
-
-  setBaseInfo = () => {
-    const { currentUser, form } = this.props;
-    if (currentUser) {
-      Object.keys(form.getFieldsValue()).forEach(key => {
-        const obj = {};
-        obj[key] = currentUser[key] || null;
-        form.setFieldsValue(obj);
-      });
-    }
-  };
 
   getAvatarURL() {
     const { currentUser } = this.props;
@@ -109,113 +88,53 @@ class BaseView extends Component<BaseViewProps> {
     this.view = ref;
   };
 
-  handlerSubmit = (event: React.MouseEvent) => {
-    event.preventDefault();
-    const { form } = this.props;
-    form.validateFields(err => {
-      if (!err) {
-        message.success(formatMessage({ id: 'accountsettings.basic.update.success' }));
-      }
-    });
+  handleFinish = () => {
+    message.success(formatMessage({ id: 'accountsettings.basic.update.success' }));
   };
 
   render() {
-    const {
-      form: { getFieldDecorator },
-    } = this.props;
+    const { currentUser } = this.props;
     return (
       <div className={styles.baseView} ref={this.getViewDom}>
         <div className={styles.left}>
-          <Form layout="vertical" hideRequiredMark>
-            <FormItem label={formatMessage({ id: 'accountsettings.basic.email' })}>
-              {getFieldDecorator('email', {
-                rules: [
-                  {
-                    required: true,
-                    message: formatMessage({ id: 'accountsettings.basic.email-message' }, {}),
-                  },
-                ],
-              })(<Input />)}
-            </FormItem>
-            <FormItem label={formatMessage({ id: 'accountsettings.basic.nickname' })}>
-              {getFieldDecorator('name', {
-                rules: [
-                  {
-                    required: true,
-                    message: formatMessage({ id: 'accountsettings.basic.nickname-message' }, {}),
-                  },
-                ],
-              })(<Input />)}
-            </FormItem>
-            {/* <FormItem label={formatMessage({ id: 'accountsettings.basic.profile' })}>
-              {getFieldDecorator('profile', {
-                rules: [
-                  {
-                    required: true,
-                    message: formatMessage({ id: 'accountsettings.basic.profile-message' }, {}),
-                  },
-                ],
-              })(
-                <Input.TextArea
-                  placeholder={formatMessage({ id: 'accountsettings.basic.profile-placeholder' })}
-                  rows={4}
-                />,
-              )}
-            </FormItem> */}
-            {/* <FormItem label={formatMessage({ id: 'accountsettings.basic.country' })}>
-              {getFieldDecorator('country', {
-                rules: [
-                  {
-                    required: true,
-                    message: formatMessage({ id: 'accountsettings.basic.country-message' }, {}),
-                  },
-                ],
-              })(
-                <Select style={{ maxWidth: 220 }}>
-                  <Option value="China">中国</Option>
-                </Select>,
-              )}
-            </FormItem> */}
-            {/* <FormItem label={formatMessage({ id: 'accountsettings.basic.geographic' })}>
-              {getFieldDecorator('geographic', {
-                rules: [
-                  {
-                    required: true,
-                    message: formatMessage({ id: 'accountsettings.basic.geographic-message' }, {}),
-                  },
-                  {
-                    validator: validatorGeographic,
-                  },
-                ],
-              })(<GeographicView />)}
-            </FormItem> */}
-            {/* <FormItem label={formatMessage({ id: 'accountsettings.basic.address' })}>
-              {getFieldDecorator('address', {
-                rules: [
-                  {
-                    required: true,
-                    message: formatMessage({ id: 'accountsettings.basic.address-message' }, {}),
-                  },
-                ],
-              })(<Input />)}
-            </FormItem> */}
-            {/* <FormItem label={formatMessage({ id: 'accountsettings.basic.phone' })}>
-              {getFieldDecorator('phone', {
-                rules: [
-                  {
-                    required: true,
-                    message: formatMessage({ id: 'accountsettings.basic.phone-message' }, {}),
-                  },
-                  { validator: validatorPhone },
-                ],
-              })(<PhoneView />)}
-            </FormItem> */}
-            <Button type="primary" onClick={this.handlerSubmit}>
-              <FormattedMessage
-                id="accountsettings.basic.update"
-                defaultMessage="Update Information"
-              />
-            </Button>
+          <Form
+            layout="vertical"
+            onFinish={this.handleFinish}
+            initialValues={currentUser}
+            hideRequiredMark
+          >
+            <Form.Item
+              name="email"
+              label={formatMessage({ id: 'accountsettings.basic.email' })}
+              rules={[
+                {
+                  required: true,
+                  message: formatMessage({ id: 'accountsettings.basic.email-message' }, {}),
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="name"
+              label={formatMessage({ id: 'accountsettings.basic.nickname' })}
+              rules={[
+                {
+                  required: true,
+                  message: formatMessage({ id: 'accountsettings.basic.nickname-message' }, {}),
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item>
+              <Button htmlType="submit" type="primary">
+                <FormattedMessage
+                  id="accountsettings.basic.update"
+                  defaultMessage="Update Information"
+                />
+              </Button>
+            </Form.Item>
           </Form>
         </div>
         <div className={styles.right}>
@@ -230,4 +149,4 @@ export default connect(
   ({ accountSettings }: { accountSettings: { currentUser: CurrentUser } }) => ({
     currentUser: accountSettings.currentUser,
   }),
-)(Form.create<BaseViewProps>()(BaseView));
+)(BaseView);
